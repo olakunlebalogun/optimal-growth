60e83d11b0072e02ab90b495446d2b7e2e1d0d8b
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-03-30 20:11:32 +0100

    worked on basic project structure and Internalization of the project

---
+++ src/main/java/com/olakunle/OptimalGrowthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.olakunle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalGrowthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimalGrowthProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olakunle/OptimalGrowthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olakunle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimalGrowthProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
