f5629a4f0fa1484fe7b390917f480bed0aa95588
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-05-17 04:46:26 +0100

    Resolved the error with on the eureka server

--- eureka-server/src/main/java/com/olakunle/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/olakunle/eurekaserver/EurekaServerApplication.java
@@ -1,15 +1,29 @@
 package com.olakunle.eurekaserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+@Slf4j
+public class EurekaServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
+
+	@Value("${eureka.instance.hostname}")
+	public String hostName;
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.error("########################## {} #############################", hostName);
+	}
+
+
 
 }
