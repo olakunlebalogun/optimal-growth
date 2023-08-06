85d2293a40365508de6de342d0fc37297ce34feb
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-08-06 12:22:38 +0100

    corrected gateway-service not registering with eureka server

--- gateway-server/src/main/java/com/olakunle/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/olakunle/gatewayserver/GatewayServerApplication.java
@@ -2,7 +2,7 @@ package com.olakunle.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 //@EnableEurekaClient
 public class GatewayServerApplication {
